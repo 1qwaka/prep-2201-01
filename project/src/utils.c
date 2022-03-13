8356e225cc1137ca22be09636b9c8da0fce6ef88
Stepanenko Andrey <yalenin01@gmail.com>
2022-03-13 17:53:13 +0300

    fixed errors showed by linters 2 (now with cpplint)

--- project/src/utils.c
+++ project/src/utils.c
@@ -9,7 +9,7 @@ size_t timer_from(unsigned char from) {
     return counter;
 }
 
-// TODO: Implement `power of` function
+// TODO(Andrey): Implement `power of` function
 /*
 int custom_pow(int base, int power) {
     return 0;
