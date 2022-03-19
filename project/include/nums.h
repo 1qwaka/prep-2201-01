216ca384286da44c7846f80401cb6928f25302f6
Stepanenko Andrey <yalenin01@gmail.com>
2022-03-19 21:09:05 +0300

    deleted counting square root in is_prime in nums.c function

--- project/include/nums.h
+++ project/include/nums.h
@@ -1,8 +1,6 @@
 #ifndef PROJECT_INCLUDE_NUMS_H_
 #define PROJECT_INCLUDE_NUMS_H_
 
-#include <math.h>
-
 int is_prime(int n);
 
 #endif  // PROJECT_INCLUDE_NUMS_H_
--- project/src/nums.c
+++ project/src/nums.c
@@ -8,8 +8,7 @@ int is_prime(int n) {
     if (n < 0)
         return 0;
 
-    int root = sqrt((double)n);
-    for (int i = 2; i < root + 1; ++i) {
+    for (int i = 2; i * i < n + 1; ++i) {
         if (n % i == 0)
             return 0;
     }
